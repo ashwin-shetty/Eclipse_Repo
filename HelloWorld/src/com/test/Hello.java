e187e9586f96002850f99ba8ca5152cb7ecbb39a
ashwin <ashwin.shetty@invenger.com>
2015-04-23 10:26:52 +0530

    Adding mycompany method

--- HelloWorld/src/com/test/Hello.java
+++ HelloWorld/src/com/test/Hello.java
@@ -4,6 +4,11 @@ public class Hello {
 
 public static void main(String[] args) {
 	System.out.println("Hello Git");
+	myCompany();
 }	
-	
+
+public static void myCompany(){
+	System.out.println("I work in invenger");
+}
+
 }

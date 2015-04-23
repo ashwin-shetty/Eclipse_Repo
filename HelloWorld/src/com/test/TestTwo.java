109943c6ca7cbaaa9b08505f7a8e1d5268504f5f
ashwin <ashwin.shetty@invenger.com>
2015-04-23 14:57:08 +0530

    Commited from comp1

--- HelloWorld/src/com/test/TestTwo.java
+++ HelloWorld/src/com/test/TestTwo.java
@@ -2,8 +2,7 @@ package com.test;
 
 public class TestTwo {
 public static void main(String[] args) {
-	System.out.println("Another File");
-	System.out.println("Test two update from comp1");
-	System.out.println("Test two update from comp2");
+	System.out.println("Test 2 File");
+	
 }
 }

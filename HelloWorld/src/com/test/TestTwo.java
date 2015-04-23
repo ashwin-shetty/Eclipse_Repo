c6b2cedd08666557d73014d06e7c23e08e0a09de
ashwin <ashwin.shetty@invenger.com>
2015-04-23 12:10:01 +0530

    Conflict handled

--- HelloWorld/src/com/test/TestTwo.java
+++ HelloWorld/src/com/test/TestTwo.java
@@ -3,6 +3,7 @@ package com.test;
 public class TestTwo {
 public static void main(String[] args) {
 	System.out.println("Another File");
+	System.out.println("Test two update from comp1");
 	System.out.println("Test two update from comp2");
 }
 }

ce151672d93042a428c537b3fbdf3dbff86de765
ashwin <ashwin.shetty@invenger.com>
2015-04-23 14:19:27 +0530

    Adding text from comp2

--- HelloWorld/src/com/test/TestTwo.java
+++ HelloWorld/src/com/test/TestTwo.java
@@ -6,5 +6,6 @@ public static void main(String[] args) {
 	System.out.println("Test two update from comp1");
 	System.out.println("Test two update from comp2");
 	System.out.println("its 12:32pm from comp1");
+	System.out.println("its 2:18pm from comp2");
 }
 }

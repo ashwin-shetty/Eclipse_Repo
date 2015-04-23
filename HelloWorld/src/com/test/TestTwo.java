9c3e8385ecc09b8d74ee5d64f8a2ec7fe8d290e9
ashwin <ashwin.shetty@invenger.com>
2015-04-23 14:25:58 +0530

    deleted few lines

--- HelloWorld/src/com/test/TestTwo.java
+++ HelloWorld/src/com/test/TestTwo.java
@@ -5,8 +5,6 @@ public static void main(String[] args) {
 	System.out.println("Another File");
 	System.out.println("Test two update from comp1");
 	System.out.println("Test two update from comp2");
-	System.out.println("its 12:32pm from comp1");
-	System.out.println("its 2:18pm from comp2");
-	System.out.println("its 2:21pm from comp1");
+
 }
 }

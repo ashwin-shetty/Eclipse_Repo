d3a9f4d40e77bd12ce8dc81b6a45f36b3039b06d
ashwin <ashwin.shetty@invenger.com>
2015-04-23 18:06:59 +0530

    Signed-off-by: ashwin <ashwin.shetty@invenger.com>

--- HelloWorld/src/com/test/TestTwo.java
+++ HelloWorld/src/com/test/TestTwo.java
@@ -3,11 +3,7 @@ package com.test;
 public class TestTwo {
 public static void main(String[] args) {
 	System.out.println("Test 2 File");
-	System.out.println("Test 2 File :3:04 :comp2");
-	System.out.println("Test 2 File :3:04 :comp2");
-	System.out.println("Test 2 File :3:02 :comp1");
-	System.out.println("From Server");
-	System.out.println("From Server");
-	System.out.println("From Server !!");
+	System.out.println("Comp1 : Test 2 File :6:06 : comp1");
+	
 }
 }

cf8c618aea3054aad1ba4f5b54ea24b5c34bdc51
Ashwin <Ashwin@Ashwin>
2018-06-11 22:06:33 +0530

    Changing print String

--- HelloWorld/src/com/test/Hello.java
+++ HelloWorld/src/com/test/Hello.java
@@ -8,7 +8,7 @@ public static void main(String[] args) {
 }	
 
 public static void myCompany(){
-	System.out.println("I work in invenger");
+	System.out.println("Java Programming");
 	System.out.println("Striving for Excellence ");
 }
 
